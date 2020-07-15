9e1112470c112b660d0009cf86a92bfd40a4d9c3
Mojo Nichols <mojo.nichols@onrr.gov>
2020-07-15 10:52:49 -0600

    Copied from doi-extractives-data/site-blog

---
+++ static/gatsby-config.js
@@ -0,0 +1,88 @@
+// Federalist provides the BASEURL env variable for preview builds.
+// https://github.com/18F/federalist-garden-build#variables-exposed-during-builds
+const BASEURL = process.env.BASEURL || '';
+
+// Federalist provides the google_analytics env variable
+const GOOGLE_ANALYTICS_ID = (process.env.google_analytics) ?
+                (process.env.google_analytics[process.env.BRANCH] || process.env.google_analytics.default)
+                :
+                "UA-48605964-8";
+
+module.exports = {
+  siteMetadata: {
+    title: 'Open data design at the Department of the Interior',
+    author: 'Ryan Johnson',
+    description: 'Our blog about data, design, and innovation at the Department of the Interior',
+    siteUrl: 'https://revenuedata.doi.gov/',
+    googleAnalyticsId: GOOGLE_ANALYTICS_ID,
+  },
+  pathPrefix: `${BASEURL}/blog`,
+  mapping: {
+    "MarkdownRemark.frontmatter.authors": `AuthorYaml`,
+  },
+  plugins: [
+    `gatsby-transformer-yaml`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/data`,
+        name: 'data',
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-autolink-headers`,
+            options: {
+              offsetY: `100`,
+              icon: `<svg aria-hidden="true" height="20" version="1.1" viewBox="0 0 16 16" width="20"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>`,
+              maintainCase: true,
+              removeAccents: true,
+            },
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 650,
+              quality: 80,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: GOOGLE_ANALYTICS_ID,
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography',
+      },
+    },
+  ],
+}
