2894cec73c5874e44eb8353ee017c5b078bcfd64
Mojo Nichols <mojo.nichols@onrr.gov>
2021-06-15 09:15:21 -0600

    Fixed newer from page 2 to page 1

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -16,7 +16,7 @@ class BlogIndex extends React.Component {
     const { currentPage, numPages } = this.props.pageContext
     const isFirst = currentPage === 1
     const isLast = currentPage === numPages
-    const prevPage = currentPage - 1 === 1 ? '/' : (currentPage - 1).toString()
+    const prevPage = currentPage - 1 === 1 ? '' : (currentPage - 1).toString()
     const nextPage = (currentPage + 1).toString()
 
     return (
